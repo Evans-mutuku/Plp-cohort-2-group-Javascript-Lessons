abfd8e5e87e2f8fb16a8024aaa429ee8b964d79c
Evans-mutuku <evansnyamai98@gmail.com>
2023-02-16 18:07:09 +0300

    first commit

---
+++ JS-INTRO/index.js
@@ -0,0 +1,24 @@
+let age  = 12
+var name = "John"
+const programmer = true
+let experience = null
+let status = undefined
+
+console.log(age, name, programmer, experience, status)
+
+
+
+let person = {
+    height : 67,
+    weight: 89
+}
+
+console.log(person)
+console.log(person.height)
+
+let student = [
+    "John", 33, true
+]
+
+console.log(student)
+console.log(student[1], student[2])
---
+++ JS-INTRO/script.js
@@ -0,0 +1,18 @@
+let x = 1
+
+while(x <= 5){
+    console.log("The value of X: " + x)
+    x++
+}
+
+
+for(var i = 5; i <= 10; i++ ){
+    console.log("Value if i is: ", i)
+}
+
+let t = 9;
+
+do{
+    console.log("Value of t is: ", t)
+    t++
+} while (t <= 20)
